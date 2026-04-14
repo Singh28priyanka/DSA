bcbfad8aae561edf85b21a39bbd91db15ad8aaf8
Priyanka singh <168950140+Singh28priyanka@users.noreply.github.com>
2026-04-15 01:27:45 +0530

    Time: 0 ms (100%), Space: 41.5 MB (99.23%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        int a=1;int b=1;
+        for(int i =0;i<n;i++){
+            int temp=a;
+            a=a+b;
+            b=temp;
+
+        }
+        return b;
+        
+    }
+}
