5903b738147cd7cc5725986ebc17394e5e14066f
Priyanka singh <168950140+Singh28priyanka@users.noreply.github.com>
2026-08-23 01:04:14 +0530

    Time: 3 ms (77.43%), Space: 46 MB (97.09%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,30 +1,27 @@
 class Solution {
-    public int romanToInt(String s) {
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        map.put('I',1);
-        map.put('V',5);
-        map.put('X',10);
-        map.put('L',50);
-        map.put('C',100);
-        map.put('D',500);
-        map.put('M',1000);
-
-        int sum = 0;
-
-        for(int i = 0; i < s.length(); i++){
-
-            int current = map.get(s.charAt(i));
-
-            if(i < s.length()-1 && current < map.get(s.charAt(i+1))){
-                sum -= current;
-            } 
-            else{
-                sum += current;
-            }
+    public int getVal(char ch){
+        switch(ch){
+            case 'I':return 1;
+            case 'V':return 5;
+            case 'X':return 10;
+            case 'L':return 50;
+            case 'C':return 100;
+            case 'D':return 500;
+            case 'M':return 1000;
+            default:return 0;
         }
-
-        return sum;
+    }
+    public int romanToInt(String s) {
+        int n =s.length();
+        int sum=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch1=s.charAt(i);
+            if((i+1)<n && getVal(ch1)<getVal(s.charAt(i+1)))
+            sum=sum-getVal(ch1);
+            else
+            sum=sum+getVal(ch1);
+        }
+         return sum;
     }
 }
